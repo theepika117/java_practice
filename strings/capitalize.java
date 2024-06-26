20350451273c659e4ae20fc3dd81988dd09704b0
theepika117 <ravikumartheepika@gmail.com>
2024-06-26 17:40:55 +0530

    updted capitalize.java

--- strings/capitalize.java
+++ strings/capitalize.java
@@ -1,16 +1,36 @@
+
+
+package strings;
 import java.io.*;
 import java.util.Scanner;
 
-package strings;
-
 public class capitalize {
+
+    static String capitalize(String str)
+    {
+        String firstLetter = str.substring(0, 1).toUpperCase();
+        String remainingString = str.substring(1);
+        return firstLetter+remainingString;
+    }
 
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
         String str ;
         String regex = "\s";
         System.out.println("Enter a String");
+        str = scr.nextLine();
+        String[] words = str.split(regex);
+        for(String word : words)
+        {
+            System.out.print(capitalize(word)+" ");
+        }
         
     }
     
 }
+/*
+ * output:
+ * Enter a String
+hello theeps how are you how was your day
+Hello Theeps How Are You How Was Your Day 
+ */
